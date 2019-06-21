7a0d6c6e23a1846b281ca5dc2703620ee5075bc1
Damola <damolasd@gmail.com>
2019-06-21 08:40:44 +0100

    Added reducer and actionCreators

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,18 +1,15 @@
+import { ERROR, GET_SMURFS, ADD_SMURF, DELETE_SMURF, UPDATE_SMURF } from '../actions';
 /*
   Be sure to import in all of the action types from `../actions`
 */
 
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
+
+//  Your initial/default state for this project could *Although does not have to* look a lot like this
+const initialState =  {
    smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
    error: null
  }
-*/
+
 
 /*
   You'll only need one smurf reducer for this project.
@@ -21,3 +18,44 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+
+export default smurfReducer = (state = initialState, action) => {
+  switch(action.type) {
+    case GET_SMURFS:
+      return {
+        ...state,
+        smurfs: action.payload,
+        error: null
+      };
+
+    case ADD_SMURF:
+        return {
+          ...state,
+          smurfs: action.payload,
+          error: null
+        };
+
+    case UPDATE_SMURF:
+        return {
+          ...state,
+          smurfs: action.payload,
+          error: null
+        };
+    
+    case DELETE_SMURF:
+        return {
+          ...state,
+          smurfs: action.payload,
+          error: null
+        };
+
+    case ERROR:
+        return {
+          ...state,
+          error: action.payload
+        };
+
+    default:
+      return state;
+  }
+}
